960d8b4991b37919649380452e646efb97d2752b
Himanshu Joshi <75315648+himanshujoshi2711@users.noreply.github.com>
2022-03-13 11:45:30 +0530

    Add files via upload

---
+++ Task1-Todolistwebapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todolist from './components/Todolist';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
